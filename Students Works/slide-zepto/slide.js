206facc81f84d71742224317534a9e44f8a69823
f-varshia <vasha.fateme@gmail.com>
2012-08-15 02:39:56 -0700

    add next function

--- Students Works/slide-zepto/slide.js
+++ Students Works/slide-zepto/slide.js
@@ -1,9 +1,13 @@
 $(function(){
 	var slidenav=$('.slideshow .slidenav li'),
 	train=$('.slideshow .train'),
+	next=$('.next');
+	previous=$('.previous');	
 	current=0;
 	
 	gotopic=function(n){
+		if(n < 0) n=slidenav.length-1; 
+		if(n >= slidenav.length) n=0; 
 		train.css({left:(-450*n)+'px'});
 		slidenav.eq(current).removeClass('active');
 		slidenav.eq(n).addClass('active');
@@ -11,5 +15,11 @@ $(function(){
 	}
 	slidenav.click(function(){
 		gotopic($(this).index());
+	});
+	next.click(function(){
+		gotopic(current+1);	
+	});
+	previous.click(function(){
+		gotopic(current-1);	
 	});
 });

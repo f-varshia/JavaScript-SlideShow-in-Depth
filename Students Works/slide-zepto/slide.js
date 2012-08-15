4fab8eea3a26c563a26ba17c258adb4988324759
f-varshia <vasha.fateme@gmail.com>
2012-08-15 02:03:38 -0700

    move slides finished

--- Students Works/slide-zepto/slide.js
+++ Students Works/slide-zepto/slide.js
@@ -1,9 +1,13 @@
 $(function(){
 	var slidenav=$('.slideshow .slidenav li'),
-	train=$('.slideshow .train');
+	train=$('.slideshow .train'),
+	current=0;
 	
 	gotopic=function(n){
 		train.css({left:(-450*n)+'px'});
+		slidenav.eq(current).removeClass('active');
+		slidenav.eq(n).addClass('active');
+		current=n;
 	}
 	slidenav.click(function(){
 		gotopic($(this).index());

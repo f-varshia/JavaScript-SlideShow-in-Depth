f3d48b586ca0ac290e5f16c2fb474d10a482ee7e
f-varshia <vasha.fateme@gmail.com>
2012-08-15 03:22:53 -0700

    better

--- Students Works/slide-zepto/slide.js
+++ Students Works/slide-zepto/slide.js
@@ -1,38 +1,35 @@
 $(function(){
 	var slidenav=$('.slideshow .slidenav li'),
-	train=$('.slideshow .train'),
-	next=$('.next');
-	previous=$('.previous'),
-	slideshow=$('.slideshow');	
+	train=$('.slideshow .train');	
 	current=0;
 	
-	gotopic=function(n){
+	(gotopic=function(n){
 		if(n < 0) n=slidenav.length-1; 
 		if(n >= slidenav.length) n=0; 
-		train.css({left:(-450*n)+'px'});
+		train.animate({left:(-450*n)+'px'},1000,'ease-in-out');
 		slidenav.eq(current).removeClass('active');
 		slidenav.eq(n).addClass('active');
 		current=n;
-	}
+	})(0);
 	slidenav.click(function(){
 		gotopic($(this).index());
 	});
-	next.click(nextf=function(){
+	$('.next').click(nextf=function(){
 		gotopic(current+1);	
 	});
-	previous.click(function(){
+	$('.previous').click(function(){
 		gotopic(current-1);
 	});
 	var iv=false;
 	(atuoplaystart=function(){
 		if(!iv)
-		iv=setInterval(nextf,1000);
+		iv=setInterval(nextf,2000);
 		return;		
 	})();
 	atuoplaystop=function(){
 		clearInterval(iv);
 		iv=false;		
 	}
-	slideshow.mouseover(atuoplaystop);
-	slideshow.mouseout(atuoplaystart);
+	$('.slideshow').mouseover(atuoplaystop);
+	$('.slideshow').mouseout(atuoplaystart);
 });

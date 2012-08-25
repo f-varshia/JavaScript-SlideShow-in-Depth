ede3acc8d9548f8bb6634af4904c7cabbb227811
f-varshia <vasha.fateme@gmail.com>
2012-08-24 23:03:06 -0700

    fix the jscript

--- Students Works/[zp] Gallery/00 php image gallery/gallery.js
+++ Students Works/[zp] Gallery/00 php image gallery/gallery.js
@@ -3,27 +3,32 @@ Math.random = function (a,b){
 	return (a && b) ? Math.floor(Math.rnd()*(b-a+1))+a : Math.rnd() ;
 }
 $(function(){
-	$images=$('div.gallery a');
-	$maxwi=window.innerWidth;
-	$maxhe=window.innerHeight;
-	$images.each(function(){
+	var images=$('div.gallery a'),
+		maxwi=window.innerWidth,
+		maxhe=window.innerHeight,
+		delay=0;
+	images.each(function(){
+	var that=this;	
+		setTimeout(function(){
+			$(that).animate({
+				left:Math.random(10,maxwi-200),
+				top:Math.random(10,maxhe-200),
+				/*rotate : (Math.random(1,360)+'deg')*/
+			},1200,'ease-in-out');
+		},delay+=300);
+	}).click(function(){
+		//===========  just to scale inside the page
+		var movex=movey=0;
+		lefti=parseInt($(this).css("left"));
+		topi=parseInt($(this).css("top"));
+		movex= (lefti <(maxwi/2)) ? 10 : -10;
+		movey= (topi <(maxhe/2)) ? 10 : -10;
+		//===========
 		$(this).animate({
-			left:Math.random(100,$maxwi-200),
-			top:Math.random(100,$maxhe-200)
-		},500);
-	});
-
-	$images.click(function(){
-		$left=parseInt($(this).css("left"));
-		$top=parseInt($(this).css("top"));
-		$movex= ($left <($maxwi/2)) ? 5 : -5;
-		$movey= ($top <($maxhe/2)) ? 5 : -5;
-		$(this).animate({
-			scale : [3, 3],
-			translateY : $movey+'px',
-			translateX : $movex+'px',
-			/*rotate: '360deg'*/
-		},2000);	
+			scale : [-3, -3],
+			rotate : ('180deg'),
+			translate :[ movex+'px',movey+'px']
+		},1200,'ease-out');	
 		
 		return false;
 	});

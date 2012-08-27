cae39fe10fb2f83eec3069383817fb9bd7f49703
f-varshia <vasha.fateme@gmail.com>
2012-08-26 22:55:13 -0700

    add script

--- Students Works/menu-zepto/menu.js
+++ Students Works/menu-zepto/menu.js
@@ -1,1 +1,11 @@
-// JavaScript Document
+$(function(){
+	var delay=0;
+	$('.menunav li').each(function(){
+		var that=this;
+		setTimeout(function(){
+			$(that).animate({
+				left:-100	
+			},500,'ease-out');	
+		},delay+=150);
+	});	
+});
